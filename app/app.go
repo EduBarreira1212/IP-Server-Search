051ff73dd4ccf87128a379569cdf58ade5f32600
Eduardo Barreira <edubarreira1212@gmail.com>
2025-06-14 21:52:20 -0300

    feat: create functionality to search a server

--- app/app.go
+++ app/app.go
@@ -13,17 +13,25 @@ func Generate() *cli.App {
 	app.Name = "CLI App"
 	app.Usage = "Search for IPs and Servers name in web"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "google.com.br",
+		},
+	}
+
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Search for IPs of websites",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "google.com.br",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Search for IPs of websites",
+			Flags:  flags,
 			Action: searchIP,
+		},
+		{
+			Name:   "server",
+			Usage:  "Search for servers of websites",
+			Flags:  flags,
+			Action: searchServer,
 		},
 	}
 
@@ -41,5 +49,19 @@ func searchIP(c *cli.Context) {
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+
+func searchServer(c *cli.Context) {
+	host := c.String("host")
+
+	servers, err := net.LookupNS(host)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, server := range servers {
+		fmt.Println(server.Host)
 	}
 }
